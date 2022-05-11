7c6c105aa0883978b0f4924057106e60af6d1342
Vitor-Veiga-Dias <75238568+Vitor-Veiga-Dias@users.noreply.github.com>
2022-05-11 02:03:06 -0300

    Feat: initial commit to react course project with typescript

---
+++ src/components/Aside/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Container } from './styled'
+
+const Aside: React.FC = () => {
+    return (
+        <Container>
+            <h1>Aside</h1>
+        </Container>
+    );
+}
+
+export default Aside;
